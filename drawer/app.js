a704b4804c6c30f441024e6a5e10dd7ca6bf243d
davidktlee <rudxor567@naver.com>
2022-03-31 16:02:29 +0900

    feat: Add all files before create new branch

--- drawer/app.js
+++ drawer/app.js
@@ -1,11 +1,31 @@
 const canvas = document.querySelector('#jsCanvas')
+const ctx = canvas.getContext('2d')
+
+ctx.fillColor
+
+let painting = false
+
+function stopPainting() {
+  painting = false
+}
 
 function onMouseMove(event) {
   const x = event.offsetX
   const y = event.offsetY
-  console.log(x, y)
 }
+
+function onMouseDown(event) {
+  painting = true
+}
+
+function onMouseUp(event) {}
 
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove)
+  canvas.addEventListener('mousedown', onMouseDown)
+  canvas.addEventListener('mouseup', onMouseUp)
+  canvas.addEventListener('mouseleave', stopPainting)
+}
+
+if (painting == true) {
 }
