aa54c4b15e3067c7656821f6ee8dc3f5286a8c3f
davidktlee <rudxor567@naver.com>
2022-03-31 17:45:29 +0900

    feat: Add feature draw function

--- drawer/app.js
+++ drawer/app.js
@@ -1,9 +1,14 @@
 const canvas = document.querySelector('#jsCanvas')
 const ctx = canvas.getContext('2d')
 
-ctx.fillColor
+ctx.strokeStyle = '#2c2c2c'
+ctx.lineWidth = 2.5
 
 let painting = false
+
+function startPainting() {
+  painting = true
+}
 
 function stopPainting() {
   painting = false
@@ -12,18 +17,23 @@ function stopPainting() {
 function onMouseMove(event) {
   const x = event.offsetX
   const y = event.offsetY
+  if (!painting) {
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+  } else {
+    ctx.lineTo(x, y)
+    ctx.stroke()
+  }
 }
 
 function onMouseDown(event) {
   painting = true
 }
 
-function onMouseUp(event) {}
-
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove)
-  canvas.addEventListener('mousedown', onMouseDown)
-  canvas.addEventListener('mouseup', onMouseUp)
+  canvas.addEventListener('mousedown', startPainting)
+  canvas.addEventListener('mouseup', stopPainting)
   canvas.addEventListener('mouseleave', stopPainting)
 }
 
