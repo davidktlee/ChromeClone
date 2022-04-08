c08dc4656c6b5c0817a2ff62697b2b62bed7860c
davidktlee <rudxor567@naver.com>
2022-04-08 20:55:24 +0900

    feat: Create hompage to section 18.
    Be blocked at the section 19.
    
    lecture's result is different from me.

---
+++ fc_starbucks/js/main.js
@@ -0,0 +1,61 @@
+const searchEl = document.querySelector('.search')
+const searchInputEl = searchEl.querySelector('input')
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus()
+})
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused')
+  searchInputEl.setAttribute('placeholder', '통합검색')
+})
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused')
+  searchInputEl.setAttribute('placeholder', '')
+})
+
+const badgeEl = document.querySelector('header .badges')
+
+window.addEventListener(
+  'scroll',
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      // gsap.to(요소, 지속시간, 옵션(객체))
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      })
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      })
+    }
+  }, 300)
+)
+// _.throttle(함수,시간)
+
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  })
+})
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  autoplay: true,
+  loop: true,
+});
+
+new Swiper('.promotion .swiper-container',{
+  slidePerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  // loop: true,
+  autoplay: {
+    // delay: 500,
+  }
+})
