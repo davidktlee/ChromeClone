81e4b7a2c0f8cd6c980a365cced9ea688fa40670
davidktlee <rudxor567@naver.com>
2022-04-12 10:38:10 +0900

    feat: End Create starbucks homepage
    Use function throttle, gsap.to, swiper

--- fc_starbucks/js/main.js
+++ fc_starbucks/js/main.js
@@ -16,6 +16,7 @@ searchInputEl.addEventListener('blur', function () {
 })
 
 const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top')
 
 window.addEventListener(
   'scroll',
@@ -26,15 +27,29 @@ window.addEventListener(
         opacity: 0,
         display: 'none',
       })
+      // 버튼 보이기
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      })
     } else {
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
         display: 'block',
+        //버튼 숨기기
+      })
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
       })
     }
   }, 300)
 )
 // _.throttle(함수,시간)
+
+toTopEl.addEventListener('click', () => {
+  gsap.to(window, 0.7, {
+    scrollTo: 0, //이걸 사용하기 위해 scrollTo 플러그인을 가져온 것
+  })
+})
 
 const fadeEls = document.querySelectorAll('.visual .fade-in')
 fadeEls.forEach(function (fadeEl, index) {
@@ -64,6 +79,60 @@ new Swiper('.promotion .swiper-container', {
   },
   navigation: {
     prevEl: '.promotion .swiper-prev',
-    nextEl: '.promotion .swiper-next'
+    nextEl: '.promotion .swiper-next',
+  },
+})
+
+new Swiper('.awards .swiper-container', {
+  slidesPerView: 5,
+
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next',
+  },
+})
+
+const promotionEl = document.querySelector('.promotion')
+const promotionToggle = document.querySelector('.toggle-promotion')
+let isHidePromotion = false
+promotionToggle.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide')
+  } else {
+    promotionEl.classList.remove('hide')
   }
 })
+
+function random(min, max) {
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay),
+  })
+}
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', 0.5, 15)
+floatingObject('.floating3', 1.5, 20)
+
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl, //보여짐 여부를 감시할 요소
+    triggerHook: 0.8,
+  })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller())
+})
+
+const thisYear = document.querySelector('.this-year')
+thisYear.textContent = new Date().getFullYear()
