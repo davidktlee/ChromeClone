29b49747b25de9eb37d6818344e0758532f6d466
davidktlee <rudxor567@naver.com>
2022-04-23 23:33:58 +0900

    feat: Create main.

--- Hanampig/js/main.js
+++ Hanampig/js/main.js
@@ -5,41 +5,52 @@ const targetNumOne = countOne.getAttribute('data-rate')
 const targetNumTwo = countTwo.getAttribute('data-rate')
 const targetNumThree = countThree.getAttribute('data-rate')
 
-let num = 0
-const timer = setInterval(() => {
-  ++num
-  countOne.innerText = num
-  if (num === 9) {
-    num = 0
-  }
-  const counter = setInterval(() => {
-    countOne.innerText = targetNumOne
-    clearInterval(timer)
-  }, 1000)
-}, 50)
+window.addEventListener(
+  'scroll',
+  _.throttle(() => {
+    if (window.scrollY < 300) {
+      let num = 0
+      const timer = setInterval(() => {
+        ++num
+        countOne.innerText = num
+        if (num === 9) {
+          num = 0
+        }
+        const counter = setInterval(() => {
+          countOne.innerText = targetNumOne
+          clearInterval(timer)
+        }, 1000)
+      }, 50)
 
-let num2 = 0
-const timer2 = setInterval(() => {
-  ++num2
-  countTwo.innerText = num2
-  if (num2 === 9) {
-    num2 = 0
-  }
-  const counter2 = setInterval(() => {
-    countTwo.innerText = targetNumTwo
-    clearInterval(timer2)
-  }, 1250)
-}, 50)
+      let num2 = 0
+      const timer2 = setInterval(() => {
+        ++num2
+        countTwo.innerText = num2
+        if (num2 === 9) {
+          num2 = 0
+        }
+        const counter2 = setInterval(() => {
+          countTwo.innerText = targetNumTwo
+          clearInterval(timer2)
+        }, 1250)
+      }, 50)
 
-let num3 = 0
-const timer3 = setInterval(() => {
-  ++num3
-  countThree.innerText = num3
-  if (num3 === 9) {
-    num3 = 0
-  }
-  const counter3 = setInterval(() => {
-    countThree.innerText = targetNumThree
-    clearInterval(timer3)
-  }, 1500)
-}, 50)
+      let num3 = 0
+      const timer3 = setInterval(() => {
+        ++num3
+        countThree.innerText = num3
+        if (num3 === 9) {
+          num3 = 0
+        }
+        const counter3 = setInterval(() => {
+          countThree.innerText = targetNumThree
+          clearInterval(timer3)
+        }, 1500)
+      }, 50)
+    } else {
+      countOne.innerText = targetNumOne
+      countTwo.innerText = targetNumTwo
+      countThree.innerText = targetNumThree
+    }
+  }, 100)
+)
